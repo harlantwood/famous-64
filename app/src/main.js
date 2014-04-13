f5f24eb0469c7ee22ac5221a33fa046f9ebaf4b6
Augustin Bralley & Harlan T Wood <pair+augie+harlan@thegoldensun.com>
2014-04-12 20:28:28 -0700

    64 surfaces

--- app/src/main.js
+++ app/src/main.js
@@ -1,28 +1,30 @@
-/*globals define*/
 define(function(require, exports, module) {
-    'use strict';
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-    var Surface = require('famous/core/Surface');
-    var Modifier = require('famous/core/Modifier');
+    var Engine     = require('famous/core/Engine');
+    var Surface    = require('famous/core/Surface');
+    var GridLayout = require('famous/views/GridLayout');
 
-    // create the main context
     var mainContext = Engine.createContext();
 
-    // your app here
-    var outline = new Surface({
-        size: [200, 200],
-        content: '<img width="200" src="' + 'content/images/famous_symbol_transparent.png' + '"/>',
-        properties: {
-            lineHeight: '200px',
-            textAlign: 'center'
-        }
+    var grid = new GridLayout({
+        dimensions: [8, 8]
     });
 
-    var outlineModifier = new Modifier({
-        origin: [0.5, 0.5]
-    });
+    var surfaces = [];
+    grid.sequenceFrom(surfaces);
 
-    mainContext.add(outlineModifier).add(outline);
+    for (var i = 0; i < 64; i++) {
+        surfaces.push(new Surface({
+            content: i + 1,
+            size: [undefined, undefined],
+            properties: {
+                backgroundColor: 'hsl(' + (i * 360 / 8) + ', 62%, 62%)',
+                color: 'black',
+                border: 'solid black',
+                lineHeight: window.innerHeight / 8 + 'px',
+                textAlign: 'center'
+            }
+        }));
+    }
 
+    mainContext.add(grid);
 });
