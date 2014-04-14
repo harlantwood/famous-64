955110a2867ed20b0a173812b6a4f9d8964d5567
Auggernaut <augustin@datacosmos.com>
2014-04-14 12:44:35 -0700

    Replaced gridview. Made boxes clickable.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,47 @@
-define(function(require, exports, module) {
-    var Engine     = require("famous/core/Engine");
-    var Surface    = require("famous/core/Surface");
-    var GridLayout = require("famous/views/GridLayout");
+define(function (require, exports, module) {
+  var Engine = require("famous/core/Engine");
+  var Surface = require("famous/core/Surface");
+  var StateModifier = require('famous/modifiers/StateModifier');
 
-    var mainContext = Engine.createContext();
+  var mainContext = Engine.createContext();
+  var x = 0, y = 0;
 
-    var grid = new GridLayout({
-        dimensions: [8, 8]
+
+  for (var i = 0; i < 64; i++) {
+    var box = new Surface({
+      content: i + 1,
+      size: [window.innerWidth / 8, window.innerHeight / 8],
+      properties: {
+        backgroundColor: "hsl(" + (i * 360 / 8) + ", 62%, 62%)",
+        color: "black",
+        border: "solid black",
+        lineHeight: window.innerHeight / 8 + 'px',
+        textAlign: 'center'
+      }
     });
 
-    var surfaces = [];
-    grid.sequenceFrom(surfaces);
-
-    for(var i = 0; i < 64; i++) {
-        surfaces.push(new Surface({
-            content: i + 1,
-            size: [undefined, undefined],
-            properties: {
-                backgroundColor: "hsl(" + (i * 360 / 8) + ", 62%, 62%)",
-                color: "black",
-                border: "solid black",
-                lineHeight: window.innerHeight / 8 + 'px',
-                textAlign: 'center'
-            }
-        }));
+    if (i != 0 && i % 8 === 0) {
+      x = 0;
+      y = y + .142
     }
 
-    mainContext.add(grid);
+    console.log("[" + x + "," + y + "]");
+
+    var modifier = new StateModifier({
+      origin: [x, y]
+    });
+
+    mainContext.add(modifier).add(box);
+
+
+    box.on('click', function() {
+      this.setProperties({
+        backgroundColor: '#878785'
+      });
+    });
+
+    x = x + .142;
+
+  }
+
 });
